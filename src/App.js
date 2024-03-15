a10c29f7da9d56321d776a01bad82b67e307542b
Arash N <arashn2y@gmail.com>
2024-03-15 12:24:28 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center">
+      <button className="bg-green-500 text-white rounded  py-2 px-4 ">Click me</button>
     </div>
   );
 }

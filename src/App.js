90a7052726b01f3a06c3b50503562af23bb8be24
Arash N <arashn2y@gmail.com>
2024-03-18 09:14:29 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="flex justify-center items-center">
-      <button className="bg-green-500 text-white rounded  py-2 px-4 ">Click me</button>
+    <div className="flex justify-center items-center h-14 bg-slate-500">
+      <button className="bg-green-500 text-white rounded py-2 px-4 ">Click me</button>
     </div>
   );
 }

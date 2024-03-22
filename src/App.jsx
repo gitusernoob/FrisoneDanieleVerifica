255473c55bf4dce55591c63fb44666738f7cd93d
Arash N <arashn2y@gmail.com>
2024-03-22 12:09:07 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,43 @@ import Header from "./components/Header";
 
 function App() {
   const [name, setName] = useState("ospite");
-  console.log("name: ", name);
-  console.log("setname", setName);
+  const [email, setEmail] = useState("example@test.com");
+  const [user, setUser] = useState({
+    name: "ospite",
+    email: "example@test.com"
+  });
+  console.log(user);
   return (
     <>
       <Header title="My App" />
+      <div className="flex flex-col justify-center items-center mt-3">
+        <input
+          type="text"
+          value={name}
+          className="border-black border-2 p-2 rounded"
+          onChange={event => {
+            const newName = event.target.value;
+            setName(newName);
+            setUser(prevState => {
+              return {
+                ...prevState,
+                name: newName
+              };
+            });
+          }}
+        />
+        <h1>{name}</h1>
+        <input
+          type="email"
+          className="border-black border-2 p-2 rounded"
+          value={email}
+          onChange={event => {
+            const email = event.target.value;
+            setEmail(email);
+          }}
+        />
+        <h1>{email}</h1>
+      </div>
     </>
   );
 }

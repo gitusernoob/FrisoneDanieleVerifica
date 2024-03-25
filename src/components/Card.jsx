0ecc30861ef6d482dfed29ede1304da8a6e849b5
ValentinaMantegna <v.alky@hotmail.it>
2024-03-25 11:27:20 +0100

    Card add

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+function Card() {
+    return (
+        <div className="w-full flex justify-center items-center relative ">
+            <div
+                className="w-2/6 flex flex-col justify-center items-center mt-5 space-y-3 py-5 px-6 rounded-lg"
+                style={{ boxShadow: "14px 5px 70px 6px rgba(0,0,0,0.1)" }}>
+                <p> test</p>
+            </div>
+        </div>
+
+    )
+}
+export default Card;

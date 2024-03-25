e60862d557e0e0877025b42aa0dda31379954dc1
Arash N <arashn2y@gmail.com>
2024-03-25 10:07:53 +0100

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -70,6 +70,9 @@ function App() {
               {passwordInputType === "text" ? <HidePassword /> : <ShowPassword />}
             </span>
           </div>
+          <div className="w-full">
+            <button className="w-full text-white bg-dark-green py-2 px-4 rounded">Login</button>
+          </div>
         </div>
       </div>
     </>

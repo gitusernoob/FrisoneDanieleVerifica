8669c1a085251108d8b0526e34cd181781f86a77
Arash N <arashn2y@gmail.com>
2024-04-04 12:07:07 +0200

    fix

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-
-function App() {
-  return <Dashboard />;
-}
-
-export default App;
---
+++ src/routes/router.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/signup",
+    element: <div>sign up</div>
+  }
+]);

c1a049109659eb71ce07d07266c2288e337db352
Arash N <arashn2y@gmail.com>
2024-04-09 09:52:30 +0200

    create todo

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,39 @@
-import Header from "../components/Header";
 import { CiLogin as Login } from "react-icons/ci";
+import { useState } from "react";
+
+import Header from "../components/Header";
+import Card from "../components/Card";
+import Input from "../components/Input";
 
 function Dashboard() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState({
+    titolo: "",
+    dataDiScadenza: "",
+    priorita: ""
+  });
+
+  const onChange = e => {
+    console.log(e.target.value);
+  };
+
   return (
     <>
-      <Header title="Dashbaord" icon={<Login />} to="/login" icon2={<Login />} to2="/login" />
+      <Header title="Dashbaord" icon={<Login />} to="/login" />
+      <Card>
+        <div className="w-full flex justify-between items-center">
+          <div>
+            <h1>Todo Done</h1>
+            <h4>keep it up</h4>
+          </div>
+          <div>{todos.length}</div>
+        </div>
+      </Card>
+      <div className="w-full flex justify-center items-center mt-5">
+        <div className="w-2/6">
+          <Input type="text" placeholder="Inserisci il task" value={todo.titolo} onChange={onChange} />
+        </div>
+      </div>
     </>
   );
 }

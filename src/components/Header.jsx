9aa0c2bbd29fbbbf440834fb22041d69fcd6fda1
Arash N <arashn2y@gmail.com>
2024-04-09 09:01:19 +0200

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,9 @@ function Header({ title, icon, to, icon2, to2 }) {
     <header className="flex justify-between items-center bg-dark-green h-16">
       <div className="w-full flex justify-center items-center relative">
         <h1 className="text-4xl text-white text-center">{title}</h1>
-        <div className="absolute right-3 flex">
+        <div className="absolute right-3">
           <Link to={to}>
             <span className="text-4xl text-white py-2 px-6 rounded">{icon}</span>
-          </Link>
-          <Link to={to2}>
-            <span className="text-4xl text-white py-2 px-6 rounded">{icon2}</span>
           </Link>
         </div>
       </div>

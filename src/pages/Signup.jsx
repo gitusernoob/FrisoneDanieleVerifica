d04ee94e0bf4538addfefd1318d7edab7aa1ce9c
DanieleFrisone <daniele.frisone@hotmail.it>
2024-04-22 10:19:27 +0200

    primo commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -84,14 +84,14 @@ function Signup() {
   function validazioneEmail(email) 
   {
     // se non ho inserito nulla nel campo
-    if(email==''){alert("Devi indicare un indirizzo email"); return false;}
+    if(email==''){setMessage("Devi indicare un indirizzo email"); return false;}
     // verifico se è un indirizzo valido
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
     {
-       alert("L'indirizzo email che hai inserito e' valido")
+      setMessage("L'indirizzo email che hai inserito e' valido")
     }
     else {
-       alert("L'indirizzo email che hai inserito non e' valido");
+      setMessage("L'indirizzo email che hai inserito non e' valido");
     }
     return false;
   }
